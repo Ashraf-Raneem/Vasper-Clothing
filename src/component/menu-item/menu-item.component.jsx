237b71e26884e7a3ea7d1018e0bc34b4d0ca65e6
Ashraf-Raneem <48803665+Ashraf-Raneem@users.noreply.github.com>
2019-12-19 05:40:42 +0600

    files structured and pictures added

---
+++ src/component/menu-item/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./menu-item.styles.scss";
+
+const MenuItem = ({title,imageUrl,size}) => (
+    <div className= {`${size} menu-item`} style={{backgroundImage: `url(${imageUrl})`}}>
+    <div className="content">
+        <h1 className="header">{title}</h1>
+        <span className="subtitle">Shop Now</span>
+    </div>
+</div>
+)
+export default MenuItem; 
