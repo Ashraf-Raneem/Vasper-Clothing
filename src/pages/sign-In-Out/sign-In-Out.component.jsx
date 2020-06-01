205683d49e6d2ac7b2f90fcc91b7b78b3949effa
Ashraf-Raneem <48803665+Ashraf-Raneem@users.noreply.github.com>
2020-06-01 10:03:06 +0600

    sign up and sign in implemented

--- src/pages/sign-In-Out/sign-In-Out.component.jsx
+++ src/pages/sign-In-Out/sign-In-Out.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react'; 
 import './sign-In-Out.styles.scss';
+
+import SignUp from '../../component/sign-up/signUp.component'
 import SignIn from '../../component/sign-in/sign-in.component'
 
 const signInAndSignOutPage = () => (
     <div className="signInAndSignOutPage"> 
     <SignIn></SignIn>
+    <SignUp></SignUp>
     </div>
 )
 export default signInAndSignOutPage; 
